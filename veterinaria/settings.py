9e23e9471166c2a723734d33eba219854202267e
Fabian Martinez <112434466+elfabito@users.noreply.github.com>
2024-12-07 19:32:17 -0300

    allowed h

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -124,8 +124,10 @@ DEFAULT_FROM_EMAIL = 'Patitas Contentas Team <noreply@patitascontentas.com>'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+
 DATABASES = {
-     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.parse(DATABASE_URL)
 }
 
 
