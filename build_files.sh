04519acbe6d2b15a0cbf04bfa271a8a5f469cf37
Fabian Martinez <112434466+elfabito@users.noreply.github.com>
2024-12-07 19:05:20 -0300

    database m

--- build_files.sh
+++ build_files.sh
@@ -17,5 +17,6 @@ pip install django-environ
 # collect static files using the Python interpreter from venv
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 echo "BUILD END"
