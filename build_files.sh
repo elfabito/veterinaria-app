2bf35d4c499db9e199b147c4b93c0282933405db
Fabian Martinez <112434466+elfabito@users.noreply.github.com>
2024-12-07 20:26:02 -0300

    all1

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,6 @@
 echo "BUILD START"
 
-# create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
 
-# activate the virtual environment
-source venv/bin/activate
-
-# install all deps in the venv
-python3.9 -m pip install pip
-
-# pip install psycopg2-binary
-# pip install python-dotenv
-
-pip install -r requirements.txt
 
 # collect static files using the Python interpreter from venv
 
