59d0cd4f02611db71a85c9ef4a5e536290c0455e
Fabian Martinez <112434466+elfabito@users.noreply.github.com>
2024-12-11 18:09:50 -0300

    whitenoise config

--- veterinaria/wsgi.py
+++ veterinaria/wsgi.py
@@ -6,12 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+from pathlib import Path
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veterinaria.settings')
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, "staticfiles_build"))
+
 app = application
