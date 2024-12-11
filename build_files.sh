c69d85c97466c29f9019e82f628ed5759a83d0ff
Fabian Martinez <112434466+elfabito@users.noreply.github.com>
2024-12-11 15:30:11 -0300

    avoid conflict same name

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,23 @@
 echo "BUILD START"
 
-# Instala las dependencias del proyecto
-python3.9 -m pip install -r requirements.txt
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
 
-# Ejecuta las migraciones
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
+python3.9 -m pip install pip
+
+# pip install psycopg2-binary
+# pip install python-dotenv
+
+pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
+
+
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-
-# Recolecta los archivos estáticos
 python manage.py collectstatic --noinput
-
 echo "BUILD END"
